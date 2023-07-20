bd27c3e06c4689db89c6a99b6478332ed8ec6ee8
Srujan Gurram <srujangs8@gmail.com>
2023-07-20 19:57:13 +0530

    🔀 Updated bun.lockb binary file.🔀 Updated package.json with axios dependency.🔀 Updated answer.ts script with axios import and added CoinpaprikaTool class.🔀 Initialized agent executor with CoinpaprikaTool and made necessary changes.🔀 Generated text and executed GraphQL query.🔀 Retrieved and displayed the required data for the answer

--- src/scripts/answer.ts
+++ src/scripts/answer.ts
@@ -1,25 +1,29 @@
 import { ChatAnthropic } from "langchain/chat_models/anthropic";
 import { HumanMessage, SystemMessage } from "langchain/schema";
 import fs from "fs";
+import axios from "axios";
+import { Tool } from "langchain/tools";
+import { initializeAgentExecutorWithOptions } from "langchain/agents";
 
 const getInput = (shortKey: string, longKey: string) => {
   const allArgs = process.argv;
   const allKeys = [shortKey, longKey];
-  if (!allArgs.some(arg => allKeys.includes(arg))) {
-    console.error(`Please provide ${shortKey} or ${longKey} as command line argument`);
+  if (!allArgs.some((arg) => allKeys.includes(arg))) {
+    console.error(
+      `Please provide ${shortKey} or ${longKey} as command line argument`
+    );
     process.exit(1);
   }
-  const index = allArgs.findIndex(arg => allKeys.includes(arg));
-  let value = '';
-  for(let i = index + 1; i < allArgs.length; i++) {
-    if(allArgs[i].startsWith('-')) break;
+  const index = allArgs.findIndex((arg) => allKeys.includes(arg));
+  let value = "";
+  for (let i = index + 1; i < allArgs.length; i++) {
+    if (allArgs[i].startsWith("-")) break;
     value += `${allArgs[i]} `;
   }
   value = value.trim();
-  console.log(`Using ${longKey.split("--")[1]} as ${value}`);
+  console.log(`🏗️ Using ${longKey.split("--")[1]} as ${value}`);
   return value;
 };
-
 
 const CLAUDE_API_KEY = getInput("-k", "--key");
 const QUERY = getInput("-q", "--query");
@@ -30,14 +34,25 @@ const typeDefs = file.toString();
 const model = new ChatAnthropic({
   temperature: 0,
   anthropicApiKey: CLAUDE_API_KEY, // In Node.js defaults to process.env.ANTHROPIC_API_KEY
-  modelName: "claude-2"
+  modelName: "claude-2",
 });
 
-const now = performance.now()
-const response = await model.generate([
-    [
-      new SystemMessage(
-        `Your task is to generate a graphql query according to the user question and given graphql type defs.
+const now = performance.now();
+
+class CoinpaprikaTool extends Tool {
+  name = "coinpaprika";
+  description = `
+  - Use this tool when the question asked by user includes price, market cap, 
+    volume, etc. of a cryptocurrency token or NFT.
+  - Input for this tool must json string with keys "question" and "original question",
+    where question is the question asked by user and original question is the question
+`;
+
+  async _call(input: string) {
+    const response = await model.generate([
+      [
+        new SystemMessage(
+          `Your task is to generate a graphql query according to the user question and given graphql type defs.
         
         Graphql Type Defs:
         ${typeDefs}
@@ -52,10 +67,44 @@ const response = await model.generate([
         }
         """
         `
-      ),
-      new HumanMessage(QUERY)
-    ],
-  ]);
+        ),
+        new HumanMessage(QUERY),
+      ],
+    ]);
 
-console.log(response.generations[0][0].text);
-console.log(`Time Taken: ${performance.now() - now}`)
+    const generatedText = response.generations[0][0].text.trim();
+
+    console.log(`\n🧬 Generated Text:\n${generatedText}`);
+
+    if (!generatedText.startsWith(`"""`) || !generatedText.endsWith(`"""`)) {
+      console.error(`Generated text does not start or end with triple quotes`);
+      process.exit(1);
+    }
+
+    const query = generatedText.replaceAll(`"""`, "").trim();
+    const { data } = await axios.post("http://localhost:3009/graphql", {
+      query,
+    });
+    const stringifiedData = JSON.stringify(data, null, 4);
+
+    return `
+      Here is the data required for your answer for question "${QUERY}":
+      ${stringifiedData}
+    `
+  }
+}
+
+
+const executor = await initializeAgentExecutorWithOptions([new CoinpaprikaTool()], model, {
+  agentType: "zero-shot-react-description",
+  verbose: true,
+  maxIterations: 8,
+  returnIntermediateSteps: true,
+});
+
+const response = await executor.call({input: QUERY});
+
+console.log(`\n✅ Answer:\n${response.output}`);
+
+
+console.log(`Time Taken: ${performance.now() - now}`);
